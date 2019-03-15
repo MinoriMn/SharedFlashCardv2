764d348079986120889fc6f9c1a427e25cdca061
MinoriMn <lucky_bluebrowneye10280915@au.com>
2019-03-15 16:49:47 +0900

    [WIP]Flash Card constructer

--- app/src/main/java/com/gmail/kamemaru2011/data/flash_card/FlashCard.kt
+++ app/src/main/java/com/gmail/kamemaru2011/data/flash_card/FlashCard.kt
@@ -22,7 +22,7 @@ class FlashCard private constructor(author: Author, flashCardId : Int, private v
             return FlashCard(author, 0x0, isUsers = true)
         }
 
-        //単語帳ロード(this user or users)
+        //単語帳ロード(this user or others)
         fun loadFlashCard(author: Author, flashCardId : Int, isUsers: Boolean) : FlashCard{
             return FlashCard(author, flashCardId, isUsers)
         }
