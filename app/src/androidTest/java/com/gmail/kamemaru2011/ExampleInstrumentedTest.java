81ec2517b9cc2120e3e51e7a7a560357f7d8c0e7
MinoriMn <lucky_bluebrowneye10280915@au.com>
2019-05-12 14:46:04 +0900

    build.gradle edited

--- app/src/androidTest/java/com/gmail/kamemaru2011/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/gmail/kamemaru2011/ExampleInstrumentedTest.java
@@ -2,8 +2,12 @@ package com.gmail.kamemaru2011;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.gmail.kamemaru2011.activity.top.TopActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,6 +20,9 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<TopActivity> topActivityActivityTestRule = new ActivityTestRule<>(TopActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
